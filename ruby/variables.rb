de450170c8921f52fc026fb9450c5929f0b48928
Stefan Caraiman <stefanc.caraiman@gmail.com>
2017-04-12 20:48:55 +0300

    Add samples for Ruby

---
+++ ruby/variables.rb
@@ -0,0 +1,8 @@
+some_int = 42
+some_float = 3.1415
+some_string = "Hello there, #{some_int}" # strings are mutable
+some_command = `pwd`
+=begin
+Multiline comment
+=end
+puts some_int, some_float, some_string, some_command
