4bf8d9981a9f80fb4e47c4e3056faf7c0cb0f405
Stefan Caraiman <Stefan.Caraiman@tss-yonder.com>
2017-04-27 16:43:11 +0300

    Add comparisons between different languages

---
+++ comparisons/python_vs_ruby/python_lang.py
@@ -0,0 +1,8 @@
+# Comment in Python
+# no multine comment syntax
+# interactive prompt can be called with "python"
+
+def say_hello(to_him):
+    print("Hello there, {}".format(to_him))
+
+say_hello("Joe")
---
+++ comparisons/python_vs_ruby/ruby_lang.rb
@@ -0,0 +1,12 @@
+# Comment in ruby
+=begin
+Multiline comment
+in Ruby
+Shell(interactive prompt) is: irb
+=end
+
+def say_hello(to_him)
+  puts "Hello there, #{to_him}"
+end
+
+say_hello("Joe")
